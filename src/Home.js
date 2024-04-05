e28032ee0eb32dad1f09a006b150ebaf0fdca5d8
emil.exenberger <emil.exenberger@gmail.com>
2024-04-05 19:20:33 +0200

    Error handling

--- src/Home.js
+++ src/Home.js
@@ -4,20 +4,32 @@ import EshopItems from './components/EshopItems';
 const Home = () => {
   const [eshopItems, setEshopItems] = useState([]);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch('http://localhost:8080/eshop/items')
-      .then(res => res.json())
+      .then(res => {
+        if(!res.ok) {
+          throw Error('Could not fetch the data for that resource');
+        }
+        return res.json()
+      })
       .then(data => {
         setEshopItems(data);
         setIsPending(false);
-      });
+        setError(null);
+      })
+      .catch(err => {
+        setIsPending(false);
+        setError(err.message);
+      })
   }, []);
 
   return (
     <div className='Home'>
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
-      {!isPending && <EshopItems eshopItems={eshopItems} />}
+      {!error && !isPending && <EshopItems eshopItems={eshopItems} />}
     </div>
   );
 }
